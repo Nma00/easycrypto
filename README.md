f4860de34e1e66cda84dc9d78b1d1e47a3effec3
Nma <fatchoumakou@free.fr>
2022-05-10 17:30:40 +0200

    update

--- README.md
+++ README.md
@@ -1,7 +1,24 @@
-# easycrypto
+# Easy Crypto
 
-Start dev server :
-npm start
+This app makes it easy and quick to buy cryptocurrencies through Transak's fiat on/off ramp integration.
 
-Production build :
+Demo site : http://easycrypto.freeddns.org
+
+## Install
+
+The application is built with [React](https://reactjs.org) and [Bootstrap](https://getbootstrap.com/) frameworks.
+
+Install dependencies :
+```
+npm install
+```
+
+Create production build :
+```
 npm run build
+```
+
+Start development server :
+```
+npm start
+```
--- src/EasycryptoApp.js
+++ src/EasycryptoApp.js
@@ -1,27 +1,10 @@
 import './EasycryptoApp.css';
 
 const TRANSAK_API_KEY = 'ff5d9cc4-647a-493e-a764-4434e063e7be';
-
-const TRANSAK_PARAMS = {
-    //widgetHeight: '625px',
-    //widgetWidth: '500px',
-    // Examples of some of the customization parameters you can pass
-    //defaultCryptoCurrency: '[DEFAULT_CRYPTO]', // Example 'ETH'
-    //walletAddress: '', // Your customer's wallet address
-    themeColor: '198754', // App theme color
-    //fiatCurrency: '', // If you want to limit fiat selection eg 'USD'
-    //email: '', // Your customer's email address
-    //redirectURL: '',
-    //hideMenu: true,
-};
 const SCREEN_TITLE = 'Buy cryptos in a few clicks';
-const BANNER = 'Easy Crypto, a quick and easy way to purchase cryptos.';
-/*
- build url avec TRANSAK_OPT
-*/
-const IFRAME_HEIGHT = 620;
+const IFRAME_HEIGHT = 630;
 const IFRAME_WIDTH = 500;
-const IFRAME_URL = `https://staging-global.transak.com?apiKey=${TRANSAK_API_KEY}&exchangeScreenTitle=${encodeURIComponent(SCREEN_TITLE)}&isFeeCalculationHidden=true&themeColor=198754&hideMenu=true`;
+const IFRAME_URL = `https://staging-global.transak.com?apiKey=${TRANSAK_API_KEY}&exchangeScreenTitle=${encodeURIComponent(SCREEN_TITLE)}&isFeeCalculationHidden=true&themeColor=198754&hideMenu=false&redirectURL=${window.location.origin}`;
 const IFRAME_STYLE = {
   display: 'block', 
   width: '100%', 
@@ -29,25 +12,32 @@ const IFRAME_STYLE = {
   maxWidth: IFRAME_WIDTH + 'px'
 }
 
-//https://staging-global.transak.com/?apiKey=[insert your staging API key here]&redirectURL=https://transak.com&cryptoCurrencyList=ETH,DAI,USDT&defaultCryptoCurrency=DAI&walletAddress=0x2dd94DC4b658F08E33272e6563dAb1758c10b1de&disableWalletAddressForm=true&exchangeScreenTitle=My%20dApp%20is%20the%20best&isFeeCalculationHidden=true
 
 function EasycryptoApp() {
   return (
-    <div className="d-flex justify-content-center">
-        <iframe height={IFRAME_HEIGHT} title="Transak On/Off Ramp Widget"
-        src={IFRAME_URL} 
-        frameBorder="no" allowtransparency="true" allowFullScreen="" 
-        style={IFRAME_STYLE}>
-        </iframe>
-
-        {/* 
-        <iframe height="625" title="Transak On/Off Ramp Widget"
-        src="https://staging-global.transak.com?apiKey=[YOUR_STAGING_API_KEY]&[QUERY_PARAMETERS]" 
-        frameborder="no" allowtransparency="true" allowfullscreen="" 
-        style="display: block; width: 100%; max-height: 625px; max-width: 500px;">
-        </iframe>
-        */}
-    </div>
+    <>
+      <nav className="navbar navbar-dark bg-success mb-5">
+        <div className="container-fluid">
+          <span className="navbar-text">
+            <a id="page-title" href="/" className="fs-2 text-light pe-3 text-decoration-none">Easy Crypto</a>
+            <span className="fs-5">A quick and easy way to purchase cryptos</span>
+           </span> 
+           <div>
+            <a href="/" className="link-light me-3" title="Easy Crypto sur Facebook"><span className="fab fa-facebook-square fa-2x"></span></a> 
+            <a href="/" className="link-light me-3" title="Easy Crypto sur Twitter"><span className="fab fa-twitter-square fa-2x"></span></a>
+            <a href="/" className="link-light me-3" title="Easy Crypto sur Telegram"><span className="fab fa-telegram fa-2x"></span></a>
+            <a href="/" className="link-light" title="Easy Crypto sur Discord"><span className="fab fa-discord fa-2x"></span></a>
+          </div>
+        </div>
+      </nav>
+      <div className="d-flex justify-content-center">
+          <iframe height={IFRAME_HEIGHT} title="Transak On/Off Ramp Widget"
+          src={IFRAME_URL} className="shadow"
+          frameBorder="no" allowtransparency="true" allowFullScreen="" 
+          style={IFRAME_STYLE}>
+          </iframe>
+      </div>
+    </>
   );
 }
 
